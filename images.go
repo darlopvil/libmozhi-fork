697ab3b7bb297e1b8571bd58166a4fc9fa9b24af
Arya Kiran <arya@projectsegfau.lt>
2024-04-14 18:40:12 +0530

    add image translation support

---
+++ images.go
@@ -0,0 +1,67 @@
+package libmozhi
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+	"github.com/tidwall/gjson"
+)
+
+type ImgOut struct {
+	SourceB64 string
+	TranslatedImgB64 string
+	SourceLang string
+	TargetLang string
+	SourceTextParsed string
+	TranslatedTextParsed string
+}
+
+func ImageGoogle(to string, from string, imgB64 string) (ImgOut, error) {
+	ToOrig := to
+	FromOrig := from
+	// For some reason google uses no for norwegian instead of nb like the rest of the translators. This is for the All function primarily
+	if to == "nb" {
+		to = "no"
+	} else if from == "nb" {
+		to = "no"
+	}
+	var ToValid bool
+	var FromValid bool
+	for _, v := range langListGoogle("sl") {
+		if v.Id == to {
+			ToValid = true
+		}
+		if v.Id == from {
+			FromValid = true
+		}
+		if FromValid == true && ToValid == true {
+			break
+		}
+	}
+	if ToValid != true {
+		return ImgOut{}, errors.New("Target Language Code invalid")
+	}
+	if FromValid != true {
+		return ImgOut{}, errors.New("Source language code invalid")
+	}
+	data := url.Values{}
+	data.Set("f.req", `[[["WqWDPb","[[\"` + string(imgB64) + `\",\"image/png\"],\"` + from + `\",\"` + to + `\"]",null,"generic"]]]`)
+	googleOut, err := postRequest("https://translate.google.com/_/TranslateWebserverUi/data/batchexecute", []byte(data.Encode()), "application/x-www-form-urlencoded")
+	if err != nil {
+		return ImgOut{}, err
+	}
+	googleOut = strings.TrimPrefix(googleOut, ")]}'")
+	if !gjson.Valid(googleOut) {
+		return ImgOut{}, errors.New("invalid json")
+	}
+	initial := gjson.Get(googleOut, "0.2").String()
+	var imgout ImgOut
+	// Thanks jsonselector.com
+	imgout.SourceB64 = imgB64
+	imgout.TranslatedImgB64 = gjson.Get(initial, "0.0").String()
+	imgout.SourceLang = FromOrig
+	imgout.TargetLang = ToOrig
+	imgout.SourceTextParsed = gjson.Get(initial, "1").String()
+	imgout.TranslatedTextParsed = gjson.Get(initial, "2").String()
+	return imgout, nil
+}
--- requests.go
+++ requests.go
@@ -2,6 +2,7 @@ package libmozhi
 
 import (
 	"bytes"
+	"strings"
 	"context"
 	"io"
 	"net/http"
@@ -17,10 +18,13 @@ func postRequest(url string, data []byte, contenttype string) (string, error) {
 		return "", err
 	}
 
-	UserAgent := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+	UserAgent := "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"
 	r.Header.Set("Content-Type", contenttype)
 	r.Header.Set("User-Agent", UserAgent)
-
+	r.Header.Set("Accept", "application/json, text/plain, */*")
+	if strings.Contains(url, "translate.google.com") {
+		r.Header["X-Goog-BatchExecute-Bgr"] = []string{`[";zdO404vQAAY15hn3FeZfsjrAbuuehnMmADQBEArZ1G-T9qS8JS9naDGjHzet_oFy3jZh71nxVmXUAikXE47SFgWlmuJyjXXQXss_cYlCHwAAADRPAAAACXUBB2MANh6IYRPOcvfOYUJskh9A8304dJA_u1Q7_ya6k8Z5quWDFm3VQiFjwsOFc7A1BrN57sTlGgX4gBcAGjiZ_U7QyQiF8546TcSILSCFIA9cRZA7hdQ-hAKS3L-4-Btym9_roHKYx4a2hKMfrWzakho2gD4J19lvIso4ot_KxS5R-hZ_IoA1vt4scD2mxm-zYypgeimoQ4Yep9SiLG-nqebyGG9FBAWZgMn8c06mzMHNqtxl_Q1z3E8KXknpQ6GKD8_mUpxpkYKsOsJCx9pxOcWU8ESjVgJhcnS8sAP33hX4GIKUC3R3Nca3HhoLTGLYaajWoI9Kaw1FlYkNKNPzWBo03k77hv8hsiAAJRMBhajAUaBRAzPL95qrpQITPKnXm9mqNx8fUcLq-6bYPOjwuUFEyClKf49UtK6d1ofIDVvqndOdSVxY_lDHvJmsEEpS4LL6xPCys1x1TZ6kxP5Rl--5YhG6s5BtQ1djOmQ4CG6qsRtNLT-fn2HOUH2yfmMzf4UGGQNVTHmnmpybIoZ7XWrRUrnjbc8HsRr6qE2i4n439o6hl7oc4LoslFKJgjn4OeS26LhYYp1VnOtZcWSd6PpsG8ypGqzgp4B82G_zkEL_Ag6U8U6UXR83ZHfBdbp0DFKDhnAdpS6snYp4g3CNMH4-ifLFvmVP22u-IIBXKogyY8Ov8txbZxngRpl7qqXG8ShSdyijM14IqLPVDjWVwkCyXMVLgiWH83EwzQ5_MmcrU1DdxZ_HKTotfY9RJpDijilj7MLbYDUTI0J8BSFqxqaDCZdXECCJ46UYGF1I7H29l9K8wy8aHvMRldqupGT_AN5n6GDkg2Zmca7elM624dzEqqW62uYiAaxznp7AAWQYO5NPe--VcnNm36x3KQ5Q2paQAx4-IqpVEHozNLhc2ma20yvFpQHJ2put8cvkxn8WHbBgs8cXhq3NnhfKbyZuJNIfAOqevqMcNJ8BTLx5j2tWw3HSQtbNpONwHw",null,null,31,null,null,null,0,"2"]`}
+	}
 	client := &http.Client{}
 	res, err0 := client.Do(r)
 	if err0 != nil {
@@ -45,7 +49,7 @@ func getRequest(url string) (string, error) {
 		return "", err
 	}
 
-	UserAgent := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+	UserAgent := "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"
 	// r.Header.Set("Content-Type", "application/json")
 	r.Header.Set("User-Agent", UserAgent)
 
